688035f4b043a08730c0be9c88219cc84f20f807
Дереженко Эвелина Викторовна EVDerezhenko <evderezhenko@stud.kpfu.ru>
2026-03-10 20:01:37 +0300

    cheking operators in query

--- task_3_bool_search.py
+++ task_3_bool_search.py
@@ -10,23 +10,37 @@ def load_index():
 
     return index
 
+
+# разбивает ввод на токены
 def tokenize_query(query):
     return re.findall(r'\(|\)|and|or|not|[а-яё]+', query.lower())
 
+
+# упорядочивает слова, учитывая приоритет операций и скобки
 def check_operators(tokenized_query):
+    print(tokenized_query)
+    output = []
+    stack = []
+    priority = {'not': 3, "and": 2, "or": 1}
     for token in tokenized_query:
+        print(token, stack, output)
         if token == '(':
-            pass
-        elif token == '(':
-            pass
-        elif token == "and":
-            pass
-        elif token == "or":
-            pass
-        elif token == "not":
-            pass
+            stack.append(token)
+        elif token == ')':
+            while stack and stack[-1] != '(':
+                output.append(stack.pop())
+            stack.pop()
+        elif token in ("and", 'or', 'not'):
+            while stack and stack[-1] != '(' and stack[-1] in priority and priority[stack[-1]] >= priority[token]:
+                output.append(stack.pop())
+            stack.append(token)
         else:
-            pass
+            output.append(token)
+    while stack:
+        print(stack)
+        output.append(stack.pop())
+    print(output)
+    return output
 
 
 INDEX_FILE = "inverted_index.txt"
