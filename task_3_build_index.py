936d13932d827dcb88c10d06c0b74c2c2aad0a83
Дереженко Эвелина Викторовна EVDerezhenko <evderezhenko@stud.kpfu.ru>
2026-03-10 18:50:32 +0300

    built inverted_index

--- task_3_build_index.py
+++ task_3_build_index.py
@@ -2,12 +2,11 @@ import os
 from collections import defaultdict
 
 LEMMAS_DIR = "lemmas"
-
+INDEX_FILE = 'inverted_index.txt'
 
 # создает словарь с леммами и названиями документов (лемма:{название_документа1, ...})
 def build_index():
     inverted_index = defaultdict(set)
-    print(inverted_index)
     for filename in os.listdir(LEMMAS_DIR):
         doc_id = filename.replace(".txt", "")
         filepath = os.path.join(LEMMAS_DIR, filename)
@@ -18,4 +17,16 @@ def build_index():
 
     return inverted_index
 
-print(build_index())
+
+# сохраняет полученный словарь в файл
+def save_index(index):
+    with open(INDEX_FILE, "w", encoding="utf-8") as f:
+        for lemma in sorted(index):
+            docs = " ".join(sorted(index[lemma]))
+            f.write(f"{lemma}: {docs}\n")
+
+
+index = build_index()
+save_index(index)
+
+print("Инвертированный индекс построен")
